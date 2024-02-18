05e8bd345692725bb4cb1fa61b80a4ab4e84a5a4
Marko Radojcic <marko.p.radojcic@gmail.com>
2024-02-18 12:47:36 +0100

    Streaming example

---
+++ yugochat-conversation-streaming.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+from openai import OpenAI
+import os
+
+class RunaAI(OpenAI):
+    @property
+    def auth_headers(self) -> dict[str, str]:
+        return {"Subscription-Key": f"{self.api_key}"}
+
+RUNAAI_SUBSCRIPTION_KEY = os.environ.get("RUNAAI_SUBSCRIPTION_KEY")
+
+client = RunaAI(
+    api_key=RUNAAI_SUBSCRIPTION_KEY,
+    base_url='https://api.runaai.com/v1',
+)
+
+#Global conversation state
+all_messages=[{
+        "role": "system",
+         "content": "Ti si asistent koji uvek hoće da pomogne.",
+        }]
+
+def process_prompt():
+    chat_completion = client.chat.completions.create(
+    messages=all_messages,
+    model="yugogpt",
+    max_tokens=512,
+    stream = True
+    )
+    
+    return chat_completion
+    
+collected_messages = []
+
+print("Pritisnite CTRL+C za izlazak.")
+while True:
+    prompt = input(">>>")
+    all_messages.append({"role" : "user", "content" : prompt})
+    chat_completion = process_prompt()
+    print("YugoGPT>>>")
+    for chunk in chat_completion:
+        print(chunk.choices[0].delta.content, end='')
+        chunk_message = chunk.choices[0].delta.content
+        collected_messages.append(chunk_message)
+    print()
+    collected_messages = [m for m in collected_messages if m is not None]
+    all_messages.append({"role" : "assistant", "content" : ''.join([m for m in collected_messages])})
+    
